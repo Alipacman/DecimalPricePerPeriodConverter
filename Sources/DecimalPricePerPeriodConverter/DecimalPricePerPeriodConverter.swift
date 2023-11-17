aace7a97e9cc38d29391709d186a3171560a081c
Ali Ebrahimi Pourasad <ali_pourasad@yahoo.de>
2023-11-17 12:26:10 +0100

    inital

--- Sources/DecimalPricePerPeriodConverter/DecimalPricePerPeriodConverter.swift
+++ Sources/DecimalPricePerPeriodConverter/DecimalPricePerPeriodConverter.swift
@@ -1,2 +1,83 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  DecimalPricePerWeekConverterTest.swift
+//  SchmusisHomeTests
+//
+//  Created by xo on 14.05.23.
+//  Copyright © 2023 Ali Ebrahimi Pourasad. All rights reserved.
+//
+
+import Foundation
+
+struct DecimalPricePerPeriodConverter {
+    // E.g. for price per week for a month -> period = 4
+    static func pricePerPeriod(from price: Decimal?, period: Int?,
+                               locale: Locale = .current,
+                               currencyCode: String? = nil) -> String {
+        guard let price = price,
+              let period = period,
+              period != 0 else { return "" }
+        
+        let weeklyPrice = CGFloat(truncating: NSDecimalNumber(decimal: price)) / CGFloat(period)
+        let roundedPrice = ceil(weeklyPrice * 100) / 100  // Rounded to two decimal places
+        return formatPrice(roundedPrice, locale: locale, currencyCode: currencyCode)
+    }
+    
+    private static func formatPrice(_ price: CGFloat, locale: Locale, currencyCode: String?) -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .currency
+        numberFormatter.maximumFractionDigits = locale.hasDecimalsInCurrency ? 2 : 0 // Adjusting the fraction digits based on the locale
+        numberFormatter.locale = locale
+        
+        if let currencyCode = currencyCode {
+            numberFormatter.currencyCode = currencyCode
+        }
+
+        let isCurrencySymbolOnLeft = locale.currencySymbolOnLeft
+        let isSpaceNeeded = locale.isSpaceNeededBetweenCurrencySymbolAndValue
+        
+        if isCurrencySymbolOnLeft {
+            numberFormatter.positiveFormat = isSpaceNeeded ? "¤ #,##0.00" : "¤#,##0.00"
+            numberFormatter.negativeFormat = isSpaceNeeded ? "-¤ #,##0.00" : "-¤#,##0.00"
+        } else {
+            numberFormatter.positiveFormat = isSpaceNeeded ? "#,##0.00 ¤" : "#,##0.00¤"
+            numberFormatter.negativeFormat = isSpaceNeeded ? "-#,##0.00 ¤" : "-#,##0.00¤"
+        }
+        
+        if let formattedPrice = numberFormatter.string(from: price as NSNumber) {
+            return formattedPrice
+        } else {
+            return "\(price)"
+        }
+    }
+}
+
+private extension Locale {
+    var currencySymbolOnLeft: Bool {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = self
+        let currencySymbol = formatter.currencySymbol ?? ""
+        let currencyCode = formatter.currencyCode ?? ""
+        let sampleString = formatter.string(from: 123 as NSNumber) ?? ""
+        return sampleString.hasPrefix(currencySymbol) || sampleString.hasPrefix(currencyCode)
+    }
+
+    var isSpaceNeededBetweenCurrencySymbolAndValue: Bool {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = self
+        let sampleString = formatter.string(from: 123 as NSNumber) ?? ""
+        let currencySymbol = formatter.currencySymbol ?? ""
+        return sampleString.contains(" \(currencySymbol)") || sampleString.contains("\(currencySymbol) ")
+    }
+    
+    // New computed property to check if the locale uses decimal places in its currency
+     var hasDecimalsInCurrency: Bool {
+         switch currencyCode {
+         case "JPY", "KRW", "BYN", "DJF", "IDR", "GNF", "KMF", "MGA", "PYG", "RWF", "VND", "VUV", "XAF", "XOF", "XPF":
+             return false
+         default:
+             return true
+         }
+     }
+}
--- Tests/DecimalPricePerPeriodConverterTests/DecimalPricePerPeriodConverterTests.swift
+++ Tests/DecimalPricePerPeriodConverterTests/DecimalPricePerPeriodConverterTests.swift
@@ -1,12 +1,202 @@
 import XCTest
 @testable import DecimalPricePerPeriodConverter
 
+// TODO Refactore with different periods
 final class DecimalPricePerPeriodConverterTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    
+    func testPricePerWeekWithNilValues() {
+        let price: Decimal? = nil
+        let period: Int? = nil
+        let result = DecimalPricePerPeriodConverter.pricePerPeriod(from: price, period: period)
+        XCTAssertEqual(result, "")
+    }
+    
+    func testPricePerWeekWithNilPrice() {
+        let price: Decimal? = nil
+        let period: Int? = 5
+        let result = DecimalPricePerPeriodConverter.pricePerPeriod(from: price, period: period)
+        XCTAssertEqual(result, "")
+    }
+    
+    func testPricePerWeekWithNilPeriod() {
+        let price: Decimal? = 500
+        let period: Int? = nil
+        let result = DecimalPricePerPeriodConverter.pricePerPeriod(from: price, period: period)
+        XCTAssertEqual(result, "")
+    }
+    
+    func testPricePerWeekWithZeroPeriod() {
+        let price: Decimal? = 500
+        let period: Int? = 0
+        let result = DecimalPricePerPeriodConverter.pricePerPeriod(from: price, period: period)
+        XCTAssertEqual(result, "")
+    }
+    
+    func testPricePerWeekWithValues() {
+        let price: Decimal? = 500
+        let period: Int? = 5
+        let locale = Locale(identifier: "en_US")
+        let result = DecimalPricePerPeriodConverter.pricePerPeriod(from: price, period: period, locale: locale, currencyCode: "USD")
+        XCTAssertEqual(result, "$100.00")
+    }
+    
+    func testPricePerWeekWithFractionalValues() {
+        let price: Decimal? = 500.50
+        let period: Int? = 7
+        let locale = Locale(identifier: "en_US")
+        let result = DecimalPricePerPeriodConverter.pricePerPeriod(from: price, period: period, locale: locale, currencyCode: "USD")
+        XCTAssertEqual(result, "$71.50")
+    }
+    
+    func testPricePerWeekWithRoundingValues() {
+        let price: Decimal? = 500.33
+        let period: Int? = 7
+        let locale = Locale(identifier: "en_US")
+        let result = DecimalPricePerPeriodConverter.pricePerPeriod(from: price, period: period, locale: locale, currencyCode: "USD")
+        XCTAssertEqual(result, "$71.48")
+    }
+    
+    func testPricePerWeekWithUSLocale() {
+        let price: Decimal? = 500.50
+        let period: Int? = 5
+        let locale = Locale(identifier: "en_US")
+        let result = DecimalPricePerPeriodConverter.pricePerPeriod(from: price, period: period, locale: locale, currencyCode: "USD")
+        XCTAssertEqual(result, "$100.10")
+    }
+    
+    func testPricePerWeekWithGermanLocale() {
+        let price: Decimal? = 500.50
+        let period: Int? = 5
+        let locale = Locale(identifier: "de_DE")
+        let result = DecimalPricePerPeriodConverter.pricePerPeriod(from: price, period: period, locale: locale, currencyCode: "EUR")
+        XCTAssertEqual(result, "100,10€")
+    }
+    
+    func testPricePerWeekWithGermanLocaleOne() {
+        let price: Decimal? = 2.00
+        let period: Int? = 2
+        let locale = Locale(identifier: "de_DE")
+        let result = DecimalPricePerPeriodConverter.pricePerPeriod(from: price, period: period, locale: locale, currencyCode: "EUR")
+        XCTAssertEqual(result, "1,00€")
+    }
+    
+    func testPricePerWeekWithGermanLocaleZeroPointNinetySix() {
+        let price: Decimal? = 4.80
+        let period: Int? = 5
+        let locale = Locale(identifier: "de_DE")
+        let result = DecimalPricePerPeriodConverter.pricePerPeriod(from: price, period: period, locale: locale, currencyCode: "EUR")
+        XCTAssertEqual(result, "0,96€")
+    }
+    
+    func testPricePerWeekWithGermanLocaleZeroPointZeroTwo() {
+        let price: Decimal? = 0.02
+        let period: Int? = 1
+        let locale = Locale(identifier: "de_DE")
+        let result = DecimalPricePerPeriodConverter.pricePerPeriod(from: price, period: period, locale: locale, currencyCode: "EUR")
+        XCTAssertEqual(result, "0,02€")
+    }
+    
+    func testPricePerWeekWithGermanLocaleOnePointZeroEight() {
+        let price: Decimal? = 5.40
+        let period: Int? = 5
+        let locale = Locale(identifier: "de_DE")
+        let result = DecimalPricePerPeriodConverter.pricePerPeriod(from: price, period: period, locale: locale, currencyCode: "EUR")
+        XCTAssertEqual(result, "1,08€")
+    }
+    
+    func testPricePerWeekWithGermanLocaleZeroPointFourFive() {
+        let price: Decimal? = 0.45
+        let period: Int? = 1
+        let locale = Locale(identifier: "de_DE")
+        let result = DecimalPricePerPeriodConverter.pricePerPeriod(from: price, period: period, locale: locale, currencyCode: "EUR")
+        XCTAssertEqual(result, "0,45€")
+    }
+    
+    func testPricePerWeekWithUSLocaleOne() {
+        let price: Decimal? = 2.00
+        let period: Int? = 2
+        let locale = Locale(identifier: "en_US")
+        let result = DecimalPricePerPeriodConverter.pricePerPeriod(from: price, period: period, locale: locale, currencyCode: "USD")
+        XCTAssertEqual(result, "$1.00")
+    }
+    
+    func testPricePerWeekWithJapaneseLocaleOne() {
+        let price: Decimal? = 2.00
+        let period: Int? = 2
+        let locale = Locale(identifier: "ja_JP")
+        let result = DecimalPricePerPeriodConverter.pricePerPeriod(from: price, period: period, locale: locale, currencyCode: "JPY")
+        XCTAssertEqual(result, "¥1.00") // In Japan, there is no fractional part for integer values
+    }
+    
+    func testPricePerWeekWithJapaneseLocaleOnePointZeroEight() {
+        let price: Decimal? = 5.40
+        let period: Int? = 5
+        let locale = Locale(identifier: "ja_JP")
+        let result = DecimalPricePerPeriodConverter.pricePerPeriod(from: price, period: period, locale: locale, currencyCode: "JPY")
+        XCTAssertEqual(result, "¥1.08")
+    }
+    
+    func testPricePerWeekWithJapaneseLocaleZeroPointFourFive() {
+        let price: Decimal? = 0.45
+        let period: Int? = 1
+        let locale = Locale(identifier: "ja_JP")
+        let result = DecimalPricePerPeriodConverter.pricePerPeriod(from: price, period: period, locale: locale, currencyCode: "JPY")
+        XCTAssertEqual(result, "¥0.45")
+    }
+    
+    func testPricePerWeekWithKoreanLocaleOne() {
+        let price: Decimal? = 2.00
+        let period: Int? = 2
+        let locale = Locale(identifier: "ko_KR")
+        let result = DecimalPricePerPeriodConverter.pricePerPeriod(from: price, period: period, locale: locale, currencyCode: "KRW")
+        XCTAssertEqual(result, "₩1.00") // In Korea, there is no fractional part for integer values
+    }
+    
+    func testPricePerWeekWithKoreanLocaleOnePointZeroEight() {
+        let price: Decimal? = 5.40
+        let period: Int? = 5
+        let locale = Locale(identifier: "ko_KR")
+        let result = DecimalPricePerPeriodConverter.pricePerPeriod(from: price, period: period, locale: locale, currencyCode: "KRW")
+        XCTAssertEqual(result, "₩1.08")
+    }
+    
+    func testPricePerWeekWithKoreanLocaleZeroPointFourFive() {
+        let price: Decimal? = 0.45
+        let period: Int? = 1
+        let locale = Locale(identifier: "ko_KR")
+        let result = DecimalPricePerPeriodConverter.pricePerPeriod(from: price, period: period, locale: locale, currencyCode: "KRW")
+        XCTAssertEqual(result, "₩0.45")
+    }
+    
+    func testPricePerWeekWithCanadianLocaleOne() {
+        let price: Decimal? = 2.00
+        let period: Int? = 2
+        let locale = Locale(identifier: "en_CA")
+        let result = DecimalPricePerPeriodConverter.pricePerPeriod(from: price, period: period, locale: locale, currencyCode: "CAD")
+        XCTAssertEqual(result, "$1.00") // In Canada, the fractional part is included even for integer values
+    }
+    
+    func testPricePerWeekWithCanadianLocaleOnePointZeroEight() {
+        let price: Decimal? = 5.40
+        let period: Int? = 5
+        let locale = Locale(identifier: "en_CA")
+        let result = DecimalPricePerPeriodConverter.pricePerPeriod(from: price, period: period, locale: locale, currencyCode: "CAD")
+        XCTAssertEqual(result, "$1.08")
+    }
+    
+    func testPricePerWeekWithCanadianLocaleZeroPointFourFive() {
+        let price: Decimal? = 0.45
+        let period: Int? = 1
+        let locale = Locale(identifier: "en_CA")
+        let result = DecimalPricePerPeriodConverter.pricePerPeriod(from: price, period: period, locale: locale, currencyCode: "CAD")
+        XCTAssertEqual(result, "$0.45")
+    }
+    
+    func testPricePerWeekWithSwissLocaleOne() {
+        let price: Decimal? = 2.00
+        let period: Int? = 2
+        let locale = Locale(identifier: "de_CH")
+        let result = DecimalPricePerPeriodConverter.pricePerPeriod(from: price, period: period, locale: locale, currencyCode: "CHF")
+        XCTAssertEqual(result, "CHF 1.00")
     }
 }
